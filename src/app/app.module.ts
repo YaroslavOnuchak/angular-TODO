514c043aa9209e1c6b2b1afd27bd500ba0a62024
Yaroslav Onuchak <onuchak.j@gmail.com>
2020-04-07 19:09:21 +0300

    create todo-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { TodosComponent } from './pages/todos/todos.component';
+import { TodoItemComponent } from './pages/todos/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    TodosComponent
+    TodosComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
